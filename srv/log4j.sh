87f0887d16e9bb7c18ffa3e0e52c7856b29cc4be
Serhii BOREMCHUK <serhii.boremchuk@smile-ukraine.com>
2022-04-13 11:11:57 +0300

    Get log4j Debian

---
+++ srv/log4j.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+lsof | grep log4j
+
+for file in $(lsof |
+    grep log4j-core |
+    awk 'match($0,/\/([^ ])*|\/([^$])*/){print substr($0,RSTART,RLENGTH)}' |
+    sort |
+    uniq); do
+    echo "$(hostname);${file};$(zipgrep -h Log4jReleaseVersion $file | cut -d ' ' -f 2)"
+done
+
+dpkg -l | grep log

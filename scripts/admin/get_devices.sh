5fda599581bfaedd25a3b5e5ed6785f287858c8d
Serhii BOREMCHUK <serhii.boremchuk@smile-ukraine.com>
2022-12-26 11:50:11 +0200

    Get local devices list with argument

--- scripts/admin/get_devices.sh
+++ scripts/admin/get_devices.sh
@@ -1,19 +1,29 @@
 #!/usr/bin/env bash
 source .env
 
+# Pass the first argument to the script
+# Example: ./get_devices.sh 10.0.0
+IPs="$1"
+
 get_devices() {
-    ip_array=()
+  # Initialize an empty array of IPs
+  ip_array=()
 
-    for i in {2..254}; do
-        IP="$IPs.$i"
-        ping "$IP" -c 1 -w 2 >/dev/null &&
-            ip_array+=("$IP") &&
-            echo "$IP"
-    done
+  for i in {2..254}; do
+    # Concatenate the IPs with the suggested range number
+    IP="$IPs.$i"
 
-    for ip in "${ip_array[@]}"; do
-        host "${ip}"
-    done
+    # Ping the IP and if it's alive, add it to the array
+    ping "$IP" -c 1 -w 2 >/dev/null &&
+      ip_array+=("$IP") &&
+      echo "$IP"
+  done
+
+  # Loop through the array and get the hostnames
+  for ip in "${ip_array[@]}"; do
+    host "${ip}"
+  done
 }
 
+# Call the function
 get_devices
